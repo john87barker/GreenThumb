2343eb70de4129eb460fdc5973fbd755a5a4878f
Jaron-Warren <Jarwarren@gmail.com>
2021-08-19 12:07:37 -0600

    backend garden and many to many gardenplant

--- GreenThumb/server/models/GardenPlants.js
+++ GreenThumb/server/models/GardenPlants.js
@@ -1,15 +1,11 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
-
+// Many to Many
 export const Garden = new Schema(
   {
-    name: { type: String, required: true },
-    body: { type: String, required: true },
-    gardenPic: { type: String },
-    hZone: { type: Number, min: 1, max: 13 },
-    zipCode: { type: Number, required: true, minlength: 5, maxlength: 5},
-    gardenPlots: { type: Array },
+    plantsId: { type: ObjectId, ref: 'Plant' },
+    gardenId: { type: ObjectId, ref: 'Garden' },
     creatorId: { type: ObjectId, required: true }
     // NOTE If you wish to add additional public properties for Accounts do so here
   },
@@ -20,5 +16,19 @@ Garden.virtual('creator', {
   foreignField: '_id',
   localField: 'creatorId',
   ref: 'Account',
+  justOne: true
+})
+
+Garden.virtual('plant', {
+  foreignField: '_id',
+  localField: 'plantsId',
+  ref: 'Plant',
+  justOne: true
+})
+
+Garden.virtual('garden', {
+  foreignField: '_id',
+  localField: 'gardenId',
+  ref: 'Garden',
   justOne: true
 })
