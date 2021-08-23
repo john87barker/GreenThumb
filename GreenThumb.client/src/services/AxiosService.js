05c982e54356009f9c8d4e58f4737090c11a4194
Shanker Karra <shankerkarra@gmail.com>
2021-08-23 13:53:36 -0600

    Added weatherApi in axios

--- GreenThumb.client/src/services/AxiosService.js
+++ GreenThumb.client/src/services/AxiosService.js
@@ -4,3 +4,13 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+export const weatherapi = Axios.create({
+  baseURL: 'http://api.weatherapi.com/v1/current.json?key=d23232cbf44a436181a183838212308&q=boise&aqi=no',
+  timeout: 10000
+})
+// REVIEW Weather API key has been created using shankerkarra@gmail.com account and it does need ZIPcode
+// at present it is has been hardcoded with boise
+
+// http://api.weatherapi.com/v1/forecast.json?key=d23232cbf44a436181a183838212308&q=83704&days=3
+//
