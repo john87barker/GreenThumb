3e1ffdd3256dcf3724c8136c5db7ddef8c34d5c6
ShereneC <spyhopper@cableone.net>
2021-08-23 16:51:12 -0600

    Working on Post - not complete - note for CJ

--- GreenThumb.client/src/services/PostsService.js
+++ GreenThumb.client/src/services/PostsService.js
@@ -11,5 +11,10 @@ class PostsService {
     const res = await api.post('api/posts', rawPost)
     AppState.posts = [res.data, ...AppState.posts]
   }
+
+  async closePost(post) {
+    const res = await api.delete('api/posts/' + post.id)
+    return res.data.postId
+  }
 }
 export const postsService = new PostsService()
