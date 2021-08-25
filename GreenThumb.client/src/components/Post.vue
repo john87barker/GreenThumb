f23d9ae6e3d0235fe20dcda09fba798eee8138dd
Jaron-Warren <Jarwarren@gmail.com>
2021-08-25 13:33:36 -0600

    fix for comment service and post update

--- GreenThumb.client/src/components/Post.vue
+++ GreenThumb.client/src/components/Post.vue
@@ -2,13 +2,10 @@
   <div class="col-md-12 text-light border-left border-top border-light p-3 mb-3 rounded shadow">
     <div class="row">
       <div class="col-md-3 d-none-sm border-right text-center">
-        <p><img :src="post.creator.picture" class="rounded-circle w-25"></p>
+        <div><img :src="post.creator.picture" class="rounded-circle w-25"></div>
         <h5>{{ post.creator.name }}</h5>
         <div class="row p-1">
           <div class="col-6 text-right" v-if="post.closed === false" title="Create Comment">
-            <button class="btn btn-warning" :data-target="'#create-comment-modal-'+ post.id" data-toggle="modal">
-              <i class="mdi mdi-plus mdi-24px"></i>
-            </button>
             <CreateCommentModal :post="post" />
           </div>
           <div class="col-6 text-right" v-if="post.closed === false && user.id === post.creatorId" title="Edit Post">
@@ -21,8 +18,16 @@
       </div>
       <div class="col-md-7 px-2 p-1">
         <h5>{{ post.title }}</h5>
-        <p> {{ post.body }} </p>
-        <p> {{ createdDate }}</p>
+        <div> {{ post.body }} </div>
+        <div>{{ createdDate }}</div>
+        <div class="ml-1 mt-4">
+          <button class="btn btn-warning" :data-target="'#create-comment-modal-'+ post.id" data-toggle="modal">
+            <i class="mdi mdi-comment-multiple mdi-24px"></i>
+          </button>
+          <button class="ml-5 btn btn-warning">
+            <i class="mdi mdi-arrow-down-thick mdi-24px"></i>
+          </button>
+        </div>
       </div>
       <div class="col-md-2">
         <div class="row">
@@ -34,9 +39,9 @@
         </div>
         <div class="row w-100 d-flex align-content-right">
           <div class="col-md-12" v-if="post.media">
-            <p>
+            <div>
               <img :src="post.media" :alt="post.creator.name" class="img-fit">
-            </p>
+            </div>
           </div>
         </div>
       </div>
