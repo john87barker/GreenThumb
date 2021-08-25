8d8aa83efccb5584a46cf9c602fa186ef90c172b
cjgreenie <seejarom@gmail.com>
2021-08-25 13:19:22 -0600

    added images

--- GreenThumb.client/src/pages/HomePage.vue
+++ GreenThumb.client/src/pages/HomePage.vue
@@ -6,11 +6,35 @@
         <span class="mx-2 text-primary">Green Thumb</span>
       </h1>
     </div> -->
-      <div class="col-md-12">
-        <div class="row mr-5">
+      <div class="col-md-12 px-5">
+        <div class="row mt-5">
           <div class="col-md-6">
           </div>
-          <div class="col-md-6 card p-3 ">
+          <div class="col-md-4 offset-1 abs right">
+            <img class=" shadow border-top border-left rounded" src="https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z2FyZGVuaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 offset-1 abs left">
+            <img class="shadow border-top border-left rounded" src="https://images.unsplash.com/photo-1581578017306-7334b15283df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2FyZGVuaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
+          </div>
+          <div class="col-md-6">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+          </div>
+          <div class="col-md-6">
+          </div>
+        </div>
+        <div class="row my-5">
+          <div class="col-md-12">
+          </div>
+        </div>
+        <div class="row abs bottom-right">
+          <div class="col-md-6">
+          </div>
+          <div class="col-md-6 card p-3 pr ">
             <div class="d-flex justify-content-around">
               <img src="../assets/img/sprout.png" alt="sprout" class="">
               <h4>Title</h4>
@@ -45,5 +69,26 @@ export default {
     height: 200px;
     width: 200px;
   }
+}
+.abs{
+  position: absolute;
+}
+.abs.right{
+  top: 80px;
+  right: 200px;
+  width: 100%;
+  height: 100%;
+}
+.abs.left{
+  top: 100px;
+  left: 5px;
+  width: 100px;
+  height: 200px;
+}
+.abs.bottom-right{
+  top: 600px;
+  right: 200px;
+  width: 70%;
+  height: 100%;
 }
 </style>
