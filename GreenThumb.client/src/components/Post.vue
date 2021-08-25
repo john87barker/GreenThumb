41035072dc51ae68e17828e4494c64802b41d7c1
Jaron-Warren <Jarwarren@gmail.com>
2021-08-25 14:35:07 -0600

    Post changes

--- GreenThumb.client/src/components/Post.vue
+++ GreenThumb.client/src/components/Post.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="col-md-12 text-light border-left border-top border-light p-3 mb-3 rounded shadow">
     <div class="row">
-      <div class="col-md-3 d-none-sm border-right text-center">
-        <div><img :src="post.creator.picture" class="rounded-circle w-25"></div>
+      <div class="col-md-3 d-none-sm border-right">
+        <div class="text-center">
+          <img :src="post.creator.picture" class="rounded-circle w-25">
+        </div>
         <h5>{{ post.creator.name }}</h5>
         <div class="row p-1">
-          <div class="col-6 text-right" v-if="post.closed === false" title="Create Comment">
+          <div class="col-6" v-if="post.closed === false" title="Create Comment">
             <CreateCommentModal :post="post" />
           </div>
-          <div class="col-6 text-right" v-if="post.closed === false && user.id === post.creatorId" title="Edit Post">
+          <div class="col-6" v-if="post.closed === false && user.id === post.creatorId" title="Edit Post">
             <button class="btn btn-warning" :data-target="'#edit-post-modal-'+post.id" data-toggle="modal">
               <i class="mdi mdi-pencil mdi-24px"></i>
             </button>
@@ -16,16 +18,29 @@
           </div>
         </div>
       </div>
-      <div class="col-md-7 px-2 p-1">
+      <div class="col-md-7 d-flex flex-column px-2 p-1">
         <h5>{{ post.title }}</h5>
+        <div v-if="post.closed">
+          Post Status: <span class="text-danger">Closed</span>
+        </div>
+        <div v-if="!post.closed">
+          Post Status: <span class="text-success">Open</span>
+        </div>
         <div> {{ post.body }} </div>
         <div>{{ createdDate }}</div>
-        <div class="ml-1 mt-4">
-          <button class="btn btn-warning" :data-target="'#create-comment-modal-'+ post.id" data-toggle="modal">
+        <div class="ml-1 mt-auto d-flex">
+          <button class="btn btn-warning" v-if="!post.closed" :data-target="'#create-comment-modal-'+ post.id" data-toggle="modal">
             <i class="mdi mdi-comment-multiple mdi-24px"></i>
           </button>
-          <button class="ml-5 btn btn-warning">
+          <button class="ml-auto btn btn-warning" v-if="!state.showComments && comments.length !== 0" @click="state.showComments=true">
             <i class="mdi mdi-arrow-down-thick mdi-24px"></i>
+            <b> Show Comments </b>
+            <i class="mdi mdi-arrow-down-thick mdi-24px"></i>
+          </button>
+          <button class="ml-auto btn btn-warning" v-if="state.showComments && comments.length !== 0" @click="state.showComments=false">
+            <i class="mdi mdi-arrow-up-thick mdi-24px"></i>
+            <b> Hide Comments </b>
+            <i class="mdi mdi-arrow-up-thick mdi-24px"></i>
           </button>
         </div>
       </div>
@@ -48,14 +63,14 @@
     </div>
   </div>
   <div class="col-md-10 offset-1 mb-5 border-bottom border-primary">
-    <div class="row">
+    <div class="row" v-if="state.showComments === true">
       <Comment v-for="c in comments" :key="c.id" :comment="c" />
     </div>
   </div>
 </template>
 
 <script>
-import { computed, watchEffect } from '@vue/runtime-core'
+import { computed, reactive, watchEffect } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import { commentsService } from '../services/CommentsService'
 import Pop from '../utils/Notifier'
@@ -78,11 +93,15 @@ export default {
         Pop.toast(error, 'error')
       }
     })
+    const state = reactive({
+      showComments: false
+    })
     watchEffect(() => props.post)
     return {
       comments: computed(() => AppState.comments[props.post.id] || []),
       user: computed(() => AppState.account),
       posts: computed(() => AppState.posts),
+      state,
 
       async closePost(post) {
         try {
@@ -113,10 +132,8 @@ export default {
         const d = new Date(props.post.createdAt)
         return new Intl.DateTimeFormat('en-US').format(d)
       })
-
     }
-  },
-  components: {}
+  }
 }
 </script>
 
