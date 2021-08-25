27464a9b6b5e2c616fda273df267e75f4becf43f
Jaron-Warren <Jarwarren@gmail.com>
2021-08-25 09:41:50 -0600

    page fix

--- GreenThumb.client/src/pages/HomePage.vue
+++ GreenThumb.client/src/pages/HomePage.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="row home flex-grow-1 d-flex flex-column align-items-end justify-content-center my-5 mt-auto">
-    <!-- <div>
+  <div class="container-fluid">
+    <div class="row home flex-grow-1 d-flex flex-column align-items-end justify-content-center my-5 mt-auto">
+      <!-- <div>
       <h1 class="col-md-12 my-5 bg-dark text-light p-3 rounded d-flex align-items-start">
         <span class="mx-2 text-primary">Green Thumb</span>
       </h1>
     </div> -->
-    <div class="col-md-12">
-      <div class="row mr-5">
-        <div class="col-md-6">
-        </div>
-        <div class="col-md-6 card p-3 ">
-          <div class="d-flex justify-content-around">
-            <img src="../assets/img/sprout.png" alt="sprout" class="">
-            <h4>Title</h4>
-            <img src="../assets/img/sprout.png" alt="sprout" class="">
+      <div class="col-md-12">
+        <div class="row mr-5">
+          <div class="col-md-6">
           </div>
-          <div class="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate similique temporibus assumenda ipsa voluptas placeat, fugiat illum alias aperiam doloremque sapiente, nihil illo consequuntur dolores ducimus commodi exercitationem nemo a.
-            <div class="d-flex justify-content-end">
-              <button type="button" class="btn btn-outline-primary m-1 ">
-                More Articles...
-              </button>
+          <div class="col-md-6 card p-3 ">
+            <div class="d-flex justify-content-around">
+              <img src="../assets/img/sprout.png" alt="sprout" class="">
+              <h4>Title</h4>
+              <img src="../assets/img/sprout.png" alt="sprout" class="">
+            </div>
+            <div class="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate similique temporibus assumenda ipsa voluptas placeat, fugiat illum alias aperiam doloremque sapiente, nihil illo consequuntur dolores ducimus commodi exercitationem nemo a.
+              <div class="d-flex justify-content-end">
+                <button type="button" class="btn btn-outline-primary m-1 ">
+                  More Articles...
+                </button>
+              </div>
             </div>
           </div>
         </div>
